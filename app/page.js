6b397416b029b8a0272fe35c7d01fd8a92c305f5
Vi <71643953+vi-ichi@users.noreply.github.com>
2023-09-28 17:10:16 +0000

    удалил компонент карты

--- app/page.js
+++ app/page.js
@@ -1,23 +1,45 @@
 "use client";
 
 import { AnimatePresence } from "framer-motion";
-import Card from "./components/Card";
 import data from "./data";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const [i, setI] = useState(0);
+  const [x, setX] = useState(0);
 
   function pop(answer, type) {
     setI(current + 1);
   }
 
+  function onDragEnd(_, info) {
+    if (info.offset.x > 0) {
+      setX(1000);
+      pop(true, type);
+      return;
+    }
+
+    if (info.offset.x < 0) {
+      setX(-1000);
+      pop(false, type);
+      return;
+    }
+  }
+
   return (
     <div className="relative flex flex-col justify-center items-center w-full h-screen gradient overflow-hidden">
       <AnimatePresence>
-        <Card type={data[i].type} pop={pop}>
+        <motion.div
+          exit={{ x }}
+          onDragEnd={onDragEnd}
+          drag
+          className={`text-4xl absolute h-[430px] w-[300px] ${
+            data[i].type === "ask" ? "bg-white text-black" : "bg-green-500 text-white"
+          } rounded-2xl flex flex-col justify-center items-center cursor-grab`}
+        >
           {data[i].content}
-        </Card>
+        </motion.div>
       </AnimatePresence>
     </div>
   );
